844ef50b9f30d052a3696a0d778e14b160a055b3
Laurent Boyer <boyerlaurent30@gmail.com>
2025-11-20 16:05:55 +0100

    saisi script

--- UE 5.2.1 Langages HTML et CSS/PHP/script.php
+++ UE 5.2.1 Langages HTML et CSS/PHP/script.php
@@ -1,17 +1,15 @@
 <?php
-// Ligne 1: Débute le bloc de code PHP
-// Ligne 2: (Vide)
-// Ligne 3: Affiche la balise <pre> pour préserver le format de print_r() [cite: 121]
+
+header('Content-Type: text/html; charset=utf-8');
+
+echo "<h1>Resultats du Formulaire</h1>";
+echo "<h2>Contenu de \$_POST (Analyse) :</h2>";
 echo "<pre>";
 
-// Ligne 4: Affiche le contenu de la variable superglobale $_POST [cite: 128]
-// La variable $_POST contient toutes les données envoyées par la méthode POST du formulaire[cite: 124, 145].
-print_r($_POST);
 
-// Ligne 5: (Ligne commentée pour ne pas afficher les données GET) [cite: 128, 146]
-// //print_r($_GET);
+var_dump($_POST);
 
-// Ligne 6: (Vide)
-// Ligne 7: Ferme la balise <pre> [cite: 129, 148]
 echo "</pre>";
+
+
 ?>
