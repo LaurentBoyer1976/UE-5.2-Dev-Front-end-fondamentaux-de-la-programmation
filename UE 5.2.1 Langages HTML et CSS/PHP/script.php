b1364d6a801b0d9e40a9d99aea9d662ecd6d13f7
Laurent Boyer <boyerlaurent30@gmail.com>
2025-11-20 16:18:07 +0100

    saisi script

--- UE 5.2.1 Langages HTML et CSS/PHP/script.php
+++ UE 5.2.1 Langages HTML et CSS/PHP/script.php
@@ -1,15 +1,9 @@
 <?php
-
+// Ligne ajoutée : Déclare le type de contenu comme HTML et l'encodage comme UTF-8
 header('Content-Type: text/html; charset=utf-8');
 
-echo "<h1>Resultats du Formulaire</h1>";
-echo "<h2>Contenu de \$_POST (Analyse) :</h2>";
 echo "<pre>";
-
-
-var_dump($_POST);
-
+print_r($_POST);
+print_r($_GET);
 echo "</pre>";
-
-
 ?>
